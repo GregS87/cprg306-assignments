d3536eba324900a32c8e979211a3803c9c8f6061
GregS87 <gregory.schultz@edu.sait.ca>
2025-09-11 11:20:58 -0600

    Week 2 Assignment Final
    Week 2 Completed
    - Added information
    week2\student-info.js
    - Imported student-info.js into week-2/page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function(){
+    return (
+        <main>
+      <ul>
+        <li>
+          Student Name: Greg Schultz
+        </li>
+        <li><Link href="https://github.com/GregS87/cprg306-assignments">My Github</Link></li>
+      </ul>
+        </main>
+    );
+}
